a4d29e0a1d9a0898f7bf5ab5af1927ef5bba5473
MOKHTAR <rrrokhtar@gmail.com>
2024-09-28 13:31:31 +0300

    feat: added new functions for configs

--- src/dynamodb-client.ts
+++ src/dynamodb-client.ts
@@ -2,11 +2,21 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 // Set the AWS Region.
 // Create an Amazon DynamoDB service client object.
-const DynamoDB_REGION = process.env.REGION;
+let DynamoDB_REGION = process.env.REGION;
 let ddbClient = new DynamoDBClient({ region: DynamoDB_REGION });
 
 const setRegion = (region: string) => {
+  DynamoDB_REGION = region;
   ddbClient = new DynamoDBClient({ region: region });
 }
 
-export { ddbClient, setRegion }
+const setAccessKey = (accessKey: { accessKeyId: string, secretAccessKey: string }) => {
+  ddbClient = new DynamoDBClient({ region: DynamoDB_REGION, credentials: accessKey });
+}
+
+const setConfig = (accessKey: { accessKeyId: string, secretAccessKey: string }, region?: string) => {
+  DynamoDB_REGION = region ?? DynamoDB_REGION;
+  ddbClient = new DynamoDBClient({ region: DynamoDB_REGION, credentials: accessKey });
+}
+
+export { ddbClient, setRegion, setAccessKey, setConfig }
