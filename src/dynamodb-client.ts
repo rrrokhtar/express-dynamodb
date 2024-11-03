2d05749acfd641640d9773b8fb945e06457e1fb3
MOKHTAR <rrrokhtar@gmail.com>
2024-11-03 10:53:19 +0200

    default values for keys and regions
    Signed-off-by: MOKHTAR <rrrokhtar@gmail.com>

--- src/dynamodb-client.ts
+++ src/dynamodb-client.ts
@@ -3,15 +3,24 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 // Set the AWS Region.
 // Create an Amazon DynamoDB service client object.
 let DynamoDB_REGION = process.env.REGION;
+let AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!;
+let AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!;
 let ddbClient = new DynamoDBClient({ region: DynamoDB_REGION });
 
 const setRegion = (region: string) => {
   DynamoDB_REGION = region;
-  ddbClient = new DynamoDBClient({ region: region });
+  ddbClient = new DynamoDBClient({ region: region, credentials: { accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY } });
 }
 
 const setAccessKey = (accessKey: { accessKeyId: string, secretAccessKey: string }) => {
-  ddbClient = new DynamoDBClient({ region: DynamoDB_REGION, credentials: accessKey });
+  AWS_ACCESS_KEY_ID = accessKey.accessKeyId;
+  AWS_SECRET_ACCESS_KEY = accessKey.secretAccessKey;
+  ddbClient = new DynamoDBClient({
+    region: DynamoDB_REGION, credentials: {
+      accessKeyId: accessKey.accessKeyId,
+      secretAccessKey: accessKey.secretAccessKey
+    }
+  });
 }
 
 const setConfig = (accessKey: { accessKeyId: string, secretAccessKey: string }, region?: string) => {
